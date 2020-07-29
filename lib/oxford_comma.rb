bdabac7e69ede300eb56a2d30dca2791836284f5
Sean B <butts.sean01@gmail.com>
2020-07-29 22:48:54 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,6 @@
 
-def oxford_comma(array)
 array = ["kiwi", "durian", "starfruit", "mangos", "dragon fruits"]
+def oxford_comma(array)
   if array.length == 2
     return "#{array[0]} and #{array[1]}"
   elsif 2 <= array.length
