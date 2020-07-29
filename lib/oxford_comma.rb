50d40adc5c13a9783ab2c9526b48decb39d3f5b7
Sean B <butts.sean01@gmail.com>
2020-07-29 22:49:09 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,36 +1,10 @@
-require 'pry'
-
-def method1
-  array[0]
-  
-end
-
-def method2
-end
 
 array = ["kiwi", "durian", "starfruit", "mangos", "dragon fruits"]
-def  array01(array)
-  array1=array[0]
-  array1
-end  
-  
-  array2 =array[0..1].to_a.join(" and ")
-  
-  
-  
-def oxford_comma(array)cc
- array = ["kiwi", "durian", "starfruit", "mangos", "dragon fruits"]
-
- array1=array[0]
- array2=array[0..1].to_a.join(" and ")
- array3=array[0..1].to_a.join(", ") + array[2]
- array1.itself
- array2.itself
-end
-  
-
 def oxford_comma(array)
-  if array.length == 2 
-    return array[0..1].to_a.join(" and ")
+  if array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  elsif 2 <= array.length
+    array[-1].insert(0, "and ")
   end
+  array.join(", ")    
 end
